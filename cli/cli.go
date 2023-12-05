8769ee004f8471824d243b50e76ef649a619af7a
arvisy <arviansyahnur3@gmail.com>
2023-12-05 18:31:41 +0700

    add main.go cli.go

---
+++ cli/cli.go
@@ -0,0 +1,37 @@
+package cli
+
+import "fmt"
+
+func ShowMainMenu() {
+	fmt.Println("1 -> Login")
+	fmt.Println("2 -> Register")
+	fmt.Println("3 -> Exit\n")
+}
+
+func ShowCustomerMenu() {
+	fmt.Println("1 -> Beli")
+	fmt.Println("2 -> Rental Pakaian")
+	fmt.Println("3 -> Pesanan")
+	fmt.Println("4 -> Edit Profil")
+	fmt.Println("5 -> Back to Main Menu\n")
+}
+
+func ShowAdminMenu() {
+	fmt.Println("1 -> Produk")
+	fmt.Println("2 -> Report")
+	fmt.Println("3 -> Back to Main Menu\n")
+}
+
+func ShowAdminProdukMenu() {
+	fmt.Println("1 -> Add Produk")
+	fmt.Println("2 -> Delete Produk")
+	fmt.Println("3 -> Update Produk")
+	fmt.Println("4 -> Back\n")
+}
+
+func ShowAdminReportMenu() {
+	fmt.Println("1 -> User Report")
+	fmt.Println("2 -> Order Report")
+	fmt.Println("3 -> Stock Report")
+	fmt.Println("4 -> Back\n")
+}
--- main.go
+++ main.go
@@ -1,5 +1,117 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"pair-project/cli"
+)
 
+func main() {
+	exitMainMenu := false
+	var choiceMainMenu int
+
+	for !exitMainMenu {
+		cli.ShowMainMenu()
+		fmt.Print("Choice: ")
+		fmt.Scan(&choiceMainMenu)
+
+		switch choiceMainMenu {
+		case 1:
+			CustomerType := ""
+			fmt.Print("Pilih Tipe Customer (Admin / Customer): ")
+			fmt.Scan(&CustomerType)
+
+			exit2 := false
+			var choiceCustomer int
+			switch CustomerType {
+			case "Customer":
+				for !exit2 {
+					cli.ShowCustomerMenu()
+					fmt.Print("Choice: ")
+					fmt.Scan(&choiceCustomer)
+
+					switch choiceCustomer {
+					case 1:
+						fmt.Println("Beli")
+					case 2:
+						fmt.Println("Rental Pakaian")
+					case 3:
+						fmt.Println("Pesanan")
+					case 4:
+						fmt.Println("Edit Profil")
+					case 5:
+						fmt.Println("Back to Main Menu")
+						exit2 = true
+					default:
+						fmt.Println("Invalid choice")
+					}
+				}
+			case "Admin":
+				for !exit2 {
+					cli.ShowAdminMenu()
+					fmt.Print("Choice: ")
+					fmt.Scan(&choiceCustomer)
+
+					switch choiceCustomer {
+					case 1:
+						var productChoice int
+						productExit := false
+
+						for !productExit {
+							cli.ShowAdminProdukMenu()
+							fmt.Print("Choice: ")
+							fmt.Scan(&productChoice)
+
+							switch productChoice {
+							case 1:
+								fmt.Println("Add Produk")
+							case 2:
+								fmt.Println("Delete Produk")
+							case 3:
+								fmt.Println("Update Produk")
+							case 4:
+								productExit = true
+							default:
+								fmt.Println("Invalid choice")
+							}
+						}
+					case 2:
+						var productChoice int
+						productExit := false
+
+						for !productExit {
+							cli.ShowAdminReportMenu()
+							fmt.Print("Choice: ")
+							fmt.Scan(&productChoice)
+
+							switch productChoice {
+							case 1:
+								fmt.Println("User Report")
+							case 2:
+								fmt.Println("Order Report")
+							case 3:
+								fmt.Println("Stock Report")
+							case 4:
+								productExit = true
+							default:
+								fmt.Println("Invalid choice")
+							}
+						}
+					case 3:
+						fmt.Println("Back to Main Menu")
+						exit2 = true
+					default:
+						fmt.Println("Invalid choice")
+					}
+				}
+			}
+
+		case 2:
+			fmt.Println("Register")
+		case 3:
+			fmt.Println("Thank you for ordering")
+			exitMainMenu = true
+		default:
+			fmt.Println("Invalid choice")
+		}
+	}
 }
