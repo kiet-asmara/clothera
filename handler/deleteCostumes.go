65344d56351784b2ff123d734c05ab18a3c63e15
arvisy <arviansyahnur3@gmail.com>
2023-12-07 23:19:12 +0700

    new change

--- handler/deleteCostumes.go
+++ handler/deleteCostumes.go
@@ -8,37 +8,50 @@ import (
 )
 
 func ShowProductsByCategoryCostumes(db *sql.DB) {
-	// Implementasi untuk menampilkan semua produk berdasarkan category yang user klik
-	// ...
+	for {
+		var categoryToDisplay string
+		fmt.Print("Enter the category to display (type category name): ")
+		fmt.Scan(&categoryToDisplay)
 
-	var categoryToDisplay string
-	fmt.Print("Enter the category to display (type category name): ")
-	fmt.Scan(&categoryToDisplay)
+		// Gunakan db.Query untuk mendapatkan data dari database
+		rows, err := db.Query("SELECT * FROM Costumes WHERE CostumeCategory = ?", categoryToDisplay)
 
-	// Gunakan db.Query untuk mendapatkan data dari database
-	rows, err := db.Query("SELECT * FROM Costumes WHERE CostumeCategory = ?", categoryToDisplay)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer rows.Close()
-
-	fmt.Printf("Showing products in the '%s' category...\n", categoryToDisplay)
-	fmt.Printf("%-10s %-30s %-15s %-15s %-15s\n", "ID", "Name", "Category", "Price", "Stock")
-	fmt.Println(strings.Repeat("-", 80))
-
-	for rows.Next() {
-		var id int
-		var name, category string
-		var price float64
-		var stock int
-
-		err := rows.Scan(&id, &name, &category, &price, &stock)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		fmt.Printf("%-10d %-30s %-15s %-15.2f %-15d\n", id, name, category, price, stock)
+		// Periksa apakah ada hasil yang ditemukan
+		if !rows.Next() {
+			fmt.Printf("No costumes found in the '%s' category.\n", categoryToDisplay)
+			continue // Lanjut ke iterasi berikutnya jika tidak ada kostum yang ditemukan
+		}
+
+		// Tampilkan kostum jika ada hasil yang ditemukan
+		fmt.Printf("Showing costumes in the '%s' category...\n", categoryToDisplay)
+		fmt.Printf("%-10s %-30s %-15s %-15s %-15s\n", "ID", "Name", "Category", "Price", "Stock")
+		fmt.Println(strings.Repeat("-", 80))
+
+		for rows.Next() {
+			var id int
+			var name, category string
+			var price float64
+			var stock int
+
+			err := rows.Scan(&id, &name, &category, &price, &stock)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Printf("%-10d %-30s %-15s %-15.2f %-15d\n", id, name, category, price, stock)
+		}
+
+		// Tanya user apakah ingin melihat kategori kostum lainnya
+		fmt.Print("Do you want to view costumes in another category? (y/n): ")
+		var input string
+		fmt.Scan(&input)
+		if strings.ToLower(input) != "y" {
+			break // Keluar dari loop jika user tidak ingin melihat kategori kostum lainnya
+		}
 	}
 }
 
